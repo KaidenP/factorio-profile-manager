2829531d4ac1f120b9e9ad11d1442e538d13f2a8
Kaiden Prince <11036039+kaidenp@users.noreply.github.com>
2025-12-05 18:57:43 -0500

    First Qt6 Application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+pub mod cxxqt_object;
+use cxx_qt_lib::{QGuiApplication, QQmlApplicationEngine, QUrl, QColor};
 fn main() {
-    println!("Hello, world!");
+    // Create the application and engine
+    let mut app = QGuiApplication::new();
+    let mut engine = QQmlApplicationEngine::new();
+
+    // Load the QML path into the engine
+    if let Some(engine) = engine.as_mut() {
+        engine.load(&QUrl::from("qrc:/qt/qml/github/KaidenP/factorio_profile_manager/qml/main.qml"));
+    }
+
+    if let Some(engine) = engine.as_mut() {
+        // Listen to a signal from the QML Engine
+        engine
+            .as_qqmlengine()
+            .on_quit(|_| {
+                println!("QML Quit!");
+            })
+            .release();
+    }
+
+    // Start the app
+    if let Some(app) = app.as_mut() {
+        app.exec();
+    }
 }
